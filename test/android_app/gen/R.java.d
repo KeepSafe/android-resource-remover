2194a7cfa65fd697d110c5e7281b74dcf1d31ae5
ca77y <ca77y@live.com>
2014-02-14 18:23:42 +0100

    Make launcher icon unused

--- test/android_app/gen/R.java.d
+++ test/android_app/gen/R.java.d
@@ -1,8 +1,23 @@
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\gen\com\example\android_app\R.java \
- : D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable-hdpi\ic_launcher.png \
+ : D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\anim\anim_missing.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\animator\animator_missing.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\color\color_missing.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable\drawable_missing.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable-hdpi\ic_launcher.png \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable-ldpi\ic_launcher.png \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable-mdpi\ic_launcher.png \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\drawable-xhdpi\ic_launcher.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\layout\layout_missing.xml \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\layout\main.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\menu\menu_missing.xml \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\values\strings.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\res\xml\xml_missing.xml \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-hdpi\ic_launcher.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-hdpi\missing.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-ldpi\ic_launcher.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-ldpi\missing.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-mdpi\ic_launcher.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-mdpi\missing.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-xhdpi\ic_launcher.png \
+D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\res\drawable-xhdpi\missing.png \
 D:\Workspace\keepsafe\android-resource-remover\test\android_app\bin\AndroidManifest.xml \
